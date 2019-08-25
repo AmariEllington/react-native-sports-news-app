df5ed8ea6e35123e810effc05726de418475881a
AmariEllington <amari.l.ellington@gmail.com>
2019-08-25 11:41:34 +0100

    text center

--- react-native-sports-news-app/src/components/NewsCard.js
+++ react-native-sports-news-app/src/components/NewsCard.js
@@ -2,16 +2,28 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 
 const NewsCard = props => {
-  const { title, urlToImage } = props.item;
+  const { title, urlToImage, url } = props.item;
 
   return (
-    <View>
-      <Text>{title}</Text>
-      <Image source={{ uri: urlToImage }} style={{ width: 500, height: 200 }} />
+    <View style={styles.newsCard}>
+      <Image source={{ uri: urlToImage }} style={styles.image} />
+      <Text style={styles.text}>{title}</Text>
     </View>
   );
 };
 
-const style = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 500,
+    height: 200
+  },
+  newsCard: {
+    borderWidth: 2,
+    borderColor: "#000000"
+  },
+  text: {
+    textAlign: "center"
+  }
+});
 
 export default NewsCard;
